c4f3d72556c4dc542f0c411a6ecb0f215c632374
_srujan_07 <125748305+Srujanrana07@users.noreply.github.com>
2024-03-21 23:54:04 +0530

    Shopping Management System

--- src/shop/Cart.java
+++ src/shop/Cart.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cart {
-    Items item=new Items();
-    Scanner sc=new Scanner(System.in);
+    Items item = new Items();
+    Scanner sc = new Scanner(System.in);
     ArrayList<String> cart_list = new ArrayList<String>();
     private int choice;
-    public void add_cart(){
-        Scanner sc = new Scanner(System.in);
-        choice=item.getChoice();
+    private double totalAmount = 0.0;
+    public void add_cart() {
+        choice = item.getChoice();
         System.out.println("Please Enter item name to your cart: ");
         String itemName = sc.nextLine().toLowerCase();
-        switch (choice){
+        double itemPrice = item.getItemPrice(itemName, choice);
+
+        switch (choice) {
             case 1:
                 if (item.isElectronicItem(itemName)) {
                     System.out.println(itemName + " is added to the cart.");
                     cart_list.add(itemName);
+                    totalAmount += itemPrice;
                 } else {
                     System.out.println(itemName + " is not available.");
                 }
@@ -26,6 +29,52 @@ public class Cart {
                 if (item.isGroceriesItem(itemName)) {
                     System.out.println(itemName + " is added to the cart.");
                     cart_list.add(itemName);
+                    totalAmount += itemPrice;
+                } else {
+                    System.out.println(itemName + " is not available.");
+                }
+                break;
+            case 3:
+                if (item.isMedicinesItem(itemName)) {
+                    System.out.println(itemName + " is added to the cart.");
+                    cart_list.add(itemName);
+                    totalAmount += itemPrice;
+                } else {
+                    System.out.println(itemName + " is not available.");
+                }
+                break;
+            case 4:
+                if (item.isTVsMobilesItem(itemName)) {
+                    System.out.println(itemName + " is added to the cart.");
+                    cart_list.add(itemName);
+                    totalAmount += itemPrice;
+                } else {
+                    System.out.println(itemName + " is not available.");
+                }
+                break;
+            case 5:
+                if (item.isHomeKitchensItem(itemName)) {
+                    System.out.println(itemName + " is added to the cart.");
+                    cart_list.add(itemName);
+                    totalAmount += itemPrice;
+                } else {
+                    System.out.println(itemName + " is not available.");
+                }
+                break;
+            case 6:
+                if (item.isDecorItem(itemName)) {
+                    System.out.println(itemName + " is added to the cart.");
+                    cart_list.add(itemName);
+                    totalAmount += itemPrice;
+                } else {
+                    System.out.println(itemName + " is not available.");
+                }
+                break;
+            case 7:
+                if (item.isBeautyItem(itemName)) {
+                    System.out.println(itemName + " is added to the cart.");
+                    cart_list.add(itemName);
+                    totalAmount += itemPrice;
                 } else {
                     System.out.println(itemName + " is not available.");
                 }
@@ -34,6 +83,8 @@ public class Cart {
                 System.out.println("Wrong Choice");
         }
     }
+    static double total_Amount = 0.0;
+
     public void view_cart(){
         System.out.println("Your Cart Items are:");
         int serialNumber = 1;
@@ -42,13 +93,24 @@ public class Cart {
             serialNumber++;
         }
     }
-    public void remove_cart(){
+
+
+    public void remove_cart() {
         view_cart();
         System.out.println("Enter number u want to remove : ");
         int n;
-        Scanner sc =new Scanner(System.in);
-        n=sc.nextInt();
-        cart_list.remove((n-1));
+        n = sc.nextInt();
+        totalAmount -= item.getItemPrice(cart_list.get(n - 1), choice);
+        cart_list.remove(n - 1);
     }
-}
 
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public ArrayList<String> getCartList() {
+        return cart_list;
+    }
+
+
+}
